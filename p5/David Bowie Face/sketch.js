4200ec5febcee214a9faf3afc6ee877a99c87380
PalomaL <32683864+PalomaL@users.noreply.github.com>
2017-11-09 17:19:55 -0500

    Changed name to David Bowie Face

---
+++ p5/David Bowie Face/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(1000, 1000)
+  fill(239, 212, 172) //skin
+  ellipse(800, 500, 100, 200) //right ear
+  ellipse(200, 500, 100, 200) //left ear
+  rect(400, 500, 200, 800) //neck
+ellipse(500, 500, 600, 800) //face
+rect(475, 400, 50, 225) //nose
+ellipse(500, 600, 75, 75) //nose
+fill(255, 255, 255) //white
+ellipse(650, 425, 75, 75) //eye
+ellipse(350, 425, 75, 75) //eye
+fill(118, 206, 226) //blue
+ellipse(650, 425, 50, 50) //color eye
+ellipse(350, 425, 50, 50) //color eye
+fill(0, 0, 0) //black
+ellipse(650, 425, 30, 30) //pupil
+ellipse(350, 425, 20, 20) //pupil
+fill(211, 158, 165) //pink
+ellipse(500, 730, 150, 30) //lips
+fill(124, 109, 78) //brown
+rect(610, 350, 100, 20) //right eyebrow
+rect(310, 350, 100, 20) //right eyebrow
+triangle(400, 50, 200, 300, 700, 100) //hair
+triangle(900, 250, 400, 50, 200, 200) //hair
+triangle(800, 100, 500, 300, 300, 100) //hair
+}
+
+function draw() {
+  // put drawing code here
+}
