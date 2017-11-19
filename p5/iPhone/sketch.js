5f3dd4da1ee0ef99132d0ff53756b0416f4e7b6e
PalomaL <32683864+PalomaL@users.noreply.github.com>
2017-11-19 10:51:59 -0500

    iPhone was added

---
+++ p5/iPhone/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(1000, 1000)
+rect(100, 100, 500, 800, 30);//phone body
+ellipse(350, 850, 60, 60);//home button
+fill(247, 163, 178)//pink
+rect(120, 200, 460, 600);//screen
+fill(0, 0, 0);//black
+line(300, 145, 400, 145);//microphone
+ellipse(280, 150, 10, 10);//cirlce
+fill(255, 255, 255);//white
+textSize(100);
+text("11:11",220, 350);//time
+textSize(20);
+text("Saturday, January 11", 250, 400);//date
+fill(210, 212, 216);//gray
+rect(140, 450, 420, 100, 20);//message
+fill(0, 0, 0);//black
+textSize(15);
+text("MESSAGES", 180, 480);//app
+text("Lula Boyd", 150, 505)//contact
+text("Hey!", 150, 525);//message
+fill(55, 224, 47);//green
+rect(150, 465, 20, 20, 5);//message icon
+fill(255, 255, 255);//white
+ellipse(160, 475, 15, 10)
+
+}
+
+function draw() {
+  // put drawing code here
+}
